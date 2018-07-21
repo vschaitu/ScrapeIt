36aa718878d329a09cf2432fe30e8d6fb34162f3
vschaitu <vschaitu@gmail.com>
2018-07-21 02:17:03 -0500

    update the heroku DB URL

--- server.js
+++ server.js
@@ -32,16 +32,13 @@ app.use(controller);
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapeit";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scrapeit");
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
