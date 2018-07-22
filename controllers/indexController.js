62e4a7bc6564fda35d55176819a66f859da4d629
vschaitu <vschaitu@gmail.com>
2018-07-22 03:05:49 -0500

    fixes to handle modals

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -15,12 +15,10 @@ var db = require("./../models");
 
 // Get home page 
 router.get("/", function (req, res) {
-    // Find all users
+    // Find all Article
     db.Article.find({})
-        // Specify that we want to populate the retrieved users with any associated notes
-        .populate("notes")
         .then(function (data) {
-            // If able to successfully find and associate all Users and Notes, send them back to the client
+            // After finding all Articles, render the page and send
             var isDataPresent;
             var hbsobject;
             if (data.length > 0) {
@@ -40,26 +38,24 @@ router.get("/", function (req, res) {
         });
 });
 
-
 // A GET route for scraping the website 
 router.get("/scrape", function (req, res) {
-    // First, we grab the body of the html with request
+    // First,  grab the body of the html with request
     axios.get("https://www.indiatimes.com/entertainment/")
         .then(function (response) {
 
-            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            // Then, load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
 
 
             // Now, we grab every h2 within an article tag, and do the following:
             $(".list-container ul li").each(function (i, element) {
 
-                // console.log($(this).children("figure").children("a").attr("href"));
-                // console.log($("figure > a").attr("href"));
+;
                 // Save an empty result object
                 var result = {};
 
-                // Add the text and href of every link, and save them as properties of the result object
+                // Add the title, image link and href of every link, and save them as properties of the result object
                 result.title = $("figure > a", this).attr("title");
                 result.link = $("figure > a", this).attr("href");
                 result.imgURL = $("figure > a > img", this).attr("src");
@@ -69,7 +65,6 @@ router.get("/scrape", function (req, res) {
                 db.Article.create(result)
                     .then(function (dbArticle) {
                         // View the added result in the console
-                        console.log(dbArticle);
                     })
                     .catch(function (err) {
                         // If an error occurred, send it to the client
@@ -82,14 +77,67 @@ router.get("/scrape", function (req, res) {
         });
 });
 
-// Delete document
-router.delete("/api/delete/article/:id", function (req, res) {
+
+// Get Aritcle with notes API
+router.get("/api/article/:id", function (req, res) {
+    // Find article by Id
+    db.Article.findById({ "_id": req.params.id })
+        // Specify that we want to populate the retrieved article with any associated notes
+        .populate("notes")
+        .then(function (data) {
+            // If able to successfully find and associate all send them back to the client
+            res.json(data);
+        })
+        .catch(function (err) {
+            // If an error occurs, send it back to the client
+            res.json(err);
+        });
+});
+
+// Add Notes to an Aritcle 
+router.post("/api/article/:id", function (req, res) {
+    // crate new note
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { notes: dbNote._id } }, { new: true });
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Delete article document
+router.delete("/api/note/:id", function (req, res) {
     // Find all users
+    db.Note.findByIdAndRemove({"_id": req.params.id })
+        .then(function (dbNote) {
+            // If a Note deleted successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be remove the reference of deleted notes_id
+            return db.Article.findOneAndUpdate({ notes: req.params.id }, { $pull: { notes: req.params.id } }, { new: true });
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+
+// Delete document
+router.delete("/api/article/:id", function (req, res) {
+    // Find article and delete
     db.Article.findByIdAndRemove({"_id": req.params.id})
         .then(function (data) {
             // If able to successfully delete send them back to the client
             res.json(data);
-            console.log(data);
         })
         .catch(function (err) {
             // If an error occurs, send it back to the client
--- public/js/scrapper.js
+++ public/js/scrapper.js
@@ -1,7 +1,7 @@
 // Once the DOM is fully loaded.
 $(function () {
 
-    // on submit , post the request
+    // on submit scrape button, route to scrap where server does the processing and scarpes and refershes page
 
     $("#scrape").on("click", function (event) {
         // Make sure to preventDefault on a submit event.
@@ -12,14 +12,15 @@ $(function () {
 
     });
 
+    // on deleteing a article, call Delete API to remove the article.
     $(".delete").on("click", function (event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
         // call API to delete the document
         let saved = $(this)
-        let id = saved.parents('div.newcard').attr("data-id");
-        $.ajax("/api/delete/article/" + id, {
+        let docid = saved.parents('div.newcard').attr("data-id");
+        $.ajax("/api/article/" + docid, {
             type: "DELETE"
         })
             .then(
@@ -29,4 +30,97 @@ $(function () {
                 });
     });
 
+    // on clicking the notes, execute load modal funcntion which inturn calls API to get document details
+    $(".notes").on("click", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        // call API to delete the document
+        let saved = $(this)
+        let docid = saved.parents('div.newcard').attr("data-id");
+
+        loadModal(docid);
+
+    });
+
+    // on clicking add note , add note to the Article by calling post API 
+    $("#addnote").on("click", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        // call API to Add a note
+        let saved = $(this)
+        let docid = $('#eModal').attr("data-id");
+        $.ajax("/api/article/" + docid, {
+            type: "POST",
+            data: {
+                body: $("#message-text").val().trim()
+            }
+        })
+            .then(
+                function (resp) {
+                    // load the modal with new note
+                    loadModal(resp._id)
+                });
+    });
+
+    // on clicking delete each note, call API to delet the note
+    $(document).on("click",".delnotes", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+      
+        // call API to delete the document
+        let saved = $(this)
+        let noteid = saved.parents('div.notes').attr("data-id");
+        
+        $.ajax("/api/note/" + noteid, {
+            type: "DELETE"
+        })
+            .then(
+                function (resp) {
+                    // load the modal with new note
+                    loadModal(resp._id)
+                });
+    });
+
 });
+
+// call the api to get the article along with notes details and render the modal and show
+function loadModal(docid) {
+
+    $.ajax("/api/article/" + docid, {
+        type: "GET"
+    })
+        .then(
+            function (data) {
+
+                $('#eModal').attr("data-id", data._id);
+
+                $("#message-text").val('');
+                $('.modal-title').html('');
+                let description = "Notes for: " + data.title.split(' ').slice(0, 3).join(' ') + '...';
+                $('.modal-title').html(description);
+
+                $('#notesection').empty();
+
+                if (data.notes.length === 0) {
+                    $('#dnotes').css("display", "block");
+                } else {
+                    $('#dnotes').css("display", "none");
+                    data.notes.forEach(i => {
+                        let div = $('<div class="notes text-info row">');
+                        let p = $('<h6 class="col-sm-11">');
+                        let b = $('<button class="delnotes col-sm-1">');
+                        let s = $('<span>&times;</span>')
+                        p.text(i.body);
+                        b.append(s);
+                        div.attr("data-id", i._id);
+                        div.append(p).append(b);
+                        $('#notesection').append(div);
+                    });
+                }
+
+                $('#eModal').modal('show');
+
+            });
+};
